9d331f60c5654120416d7500dbfa5ed5db0f307d
camilazgomez <cazumaeta1@miuandes.cl>
2025-07-08 20:19:05 -0400

    feat: added call to webhook to notify available results

--- services/task_runner.py
+++ services/task_runner.py
@@ -1,11 +1,15 @@
 from typing import List, Dict
 from services.scraper_service import scrape_category
 from services.gsheet_service  import upload_rows_to_gsheet
+from services.webhook_service import notify_webhook   
+import os
+
+REQUEST_EMAIL = os.getenv("REQUEST_EMAIL")
 
 def run_scraping_flow(category: str, webhook: str) -> None:
     rows: List[Dict[str, str]] = scrape_category(category)
-
     sheet_links = upload_rows_to_gsheet(rows, category)
-    print(sheet_links)
+    sheet_url = sheet_links["url_to_tab"]
+    notify_webhook(webhook, REQUEST_EMAIL, sheet_url)
     
 
---
+++ services/webhook_service.py
@@ -0,0 +1,27 @@
+from typing import Final
+import logging
+import httpx
+
+logger = logging.getLogger(__name__)
+TIMEOUT: Final[int] = 10  
+
+def notify_webhook( webhook_url: str, email: str, sheet_link: str,) -> bool:
+    payload = {"email": email, "link": sheet_link}
+    try:
+        resp = httpx.post(webhook_url, json=payload, timeout=TIMEOUT)
+        resp.raise_for_status()
+        logger.info(
+            "[webhook_service] Webhook %s notified (status=%s)",
+            webhook_url,
+            resp.status_code,
+        )
+        return True
+
+    except Exception as exc:
+        logger.error(
+            "[webhook_service] Could not notify %s → %s",
+            webhook_url,
+            exc,
+            exc_info=True,
+        )
+        return False
